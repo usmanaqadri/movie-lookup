03674aebfe4e5d18ebbf4f12491a9c67cc9b1dd6
usmanaqadri <usmanaqadri@gmail.com>
2022-04-14 19:40:58 -0400

    Updates

---
+++ precache-manifest.30cf94a4016176e8863cdfc33069eefa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f803080e60663c22081",
+    "url": "/movie-lookup/static/css/main.4b389cab.chunk.css"
+  },
+  {
+    "revision": "9f803080e60663c22081",
+    "url": "/movie-lookup/static/js/main.9f803080.chunk.js"
+  },
+  {
+    "revision": "d3997781c57d2e159a83",
+    "url": "/movie-lookup/static/js/1.d3997781.chunk.js"
+  },
+  {
+    "revision": "2345e752001699b9e0ca",
+    "url": "/movie-lookup/static/js/runtime~main.2345e752.js"
+  },
+  {
+    "revision": "4a100157982494a6cd75f230c028225a",
+    "url": "/movie-lookup/index.html"
+  }
+];
